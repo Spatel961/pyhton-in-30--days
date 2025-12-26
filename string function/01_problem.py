3af2ecd93dc4ec7403e640f707f27cf0bae62675
Spatel961 <spatel34@islander.tamucc.edu>
2025-12-26 17:41:53 +0530

    Update 01_problem.py

--- string function/01_problem.py
+++ string function/01_problem.py
@@ -1,4 +1,4 @@
 # write a programe to disply a user enter name  folowed by good afternoon using input
 
 name = input("enter your name :  ")
-print(f"good afternoon {name}")
+printf("good afternoon {name}")
